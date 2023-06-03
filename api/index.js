20d1951d7777f9e5f13f9299618c412666aaf105
mari0514 <111376774+mari0514@users.noreply.github.com>
2023-06-03 00:20:29 -0700

    finished the auth part

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+// const express = require('express');
+import authRoutes from './routes/auth.js';
+import postRoutes from './routes/posts.js';
+import userRoutes from './routes/users.js';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+app.use("/api/auth", authRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/users", userRoutes);
+
+// app.get("/test", (req, res) => {
+//     res.json("It seems to be working!");
+// });
+
+app.listen(8000, () => {
+    console.log("Connected to the backend!!");
+});
