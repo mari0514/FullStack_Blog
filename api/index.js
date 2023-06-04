66a9fd21d63fd9a94f48a63e7f126c94627b0e6b
mari0514 <111376774+mari0514@users.noreply.github.com>
2023-06-04 03:13:45 -0700

    still working on backend

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import postRoutes from './routes/posts.js';
 import userRoutes from './routes/users.js';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import multer from 'multer';
 
 
 const app = express();
@@ -16,6 +17,25 @@ app.use(cors({
     credentials: true
 }));
 app.use(cookieParser());
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, '../client/public/upload');
+    },
+    filename: function (req, file, cb) {
+        cb(null, Date.now() + file.originalname);
+    }
+});
+
+const upload = multer({ storage });
+
+app.post('/api/upload', upload.single('file'), function (req, res) {
+    const file = req.file;
+    res.status(200).json(file.filename);
+    // console.log('file');
+});
+
+
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
