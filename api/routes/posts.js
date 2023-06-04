37574dc7a369b731d47c1b146aacf1d6e1f26671
mari0514 <111376774+mari0514@users.noreply.github.com>
2023-06-03 21:10:02 -0700

    working on backend

--- api/routes/posts.js
+++ api/routes/posts.js
@@ -1,9 +1,16 @@
 import express from "express";
+import { addPost, deletePost, getPost, getPosts, updatePost } from "../controllers/post.js";
 // import { addPost } from "../controllers/post.js";
 
 const router = express.Router();
 
+router.get("/", getPosts);
+// router.get("/api/posts", getPosts);
 
+router.get("/:id", getPost);
+router.post("/", addPost);
+router.delete("/:id", deletePost);
+router.put("/:id", updatePost);
 
 // router.get("/test", (req, res) => {
 //     res.json("this is a post");
