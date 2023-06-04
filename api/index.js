303074199e91b8567d13d5fa0cfe883783eb84af
mari0514 <111376774+mari0514@users.noreply.github.com>
2023-06-04 00:52:21 -0700

    getPost getPosts deletePost completed

--- api/index.js
+++ api/index.js
@@ -10,7 +10,11 @@ import cookieParser from 'cookie-parser';
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+}));
 app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/posts", postRoutes);
